6019315fe3477474a70fc5a8c590bc2c8a4614aa
Florent Clarret <florent.clarret@gmail.com>
2022-11-10 23:16:35 +0100

    feat(kubectl): add aliases

---
+++ dot_oh-my-zsh/custom/kubectl.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+alias k='kubectl'
+
+alias kci='kubectl cluster-info'
+alias kex='kubectl exec -i -t'
+alias klog='kubectl logs -f'
+alias kg='kubectl get'
+alias kgpo='kubectl get pod'
+alias kctx='kubectl config get-contexts'
+alias kcctx='kubectl config current-context'
+alias kuctx='kubectl config use-context'
+alias kdp='kubectl describe pods'
+alias kdn='kubectl describe nodes'
