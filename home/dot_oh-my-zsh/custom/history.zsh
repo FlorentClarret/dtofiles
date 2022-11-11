ca93dfdc7be36ce62a2df079bf4327a1a1da10f9
Florent Clarret <florent.clarret@gmail.com>
2022-11-11 09:38:24 +0100

    feat(history): drop shopt

--- home/dot_oh-my-zsh/custom/history.zsh
+++ home/dot_oh-my-zsh/custom/history.zsh
@@ -4,6 +4,3 @@ export HISTTIMEFORMAT="%d/%m/%y %T "
 export HISTCONTROL=ignoreboth:erasedups
 export HISTSIZE=8192
 export HISTFILESIZE=8192
-
-# Append to the history file instead of overwrite
-shopt -s histappend
