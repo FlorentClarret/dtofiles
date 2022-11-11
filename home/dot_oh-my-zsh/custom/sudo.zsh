f64a5ac5cb4ecc4fd1e7a5c7380429126cfe3c2e
Florent Clarret <florent.clarret@gmail.com>
2022-11-11 08:35:00 +0100

    feat(sudo): add aliases

---
+++ home/dot_oh-my-zsh/custom/sudo.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+# One alias to use them all
+alias sudoa='sudo '
+# Seems useless? If the last character of the alias value is a blank, then the next command word following the alias is also checked for alias expansion.
