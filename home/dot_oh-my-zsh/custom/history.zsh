2b29cae7f43230279f21695a2aa4bddeef0e2ca0
Florent Clarret <florent.clarret@gmail.com>
2022-11-11 15:35:54 +0100

    feat(history): update config

--- home/dot_oh-my-zsh/custom/history.zsh
+++ home/dot_oh-my-zsh/custom/history.zsh
@@ -2,5 +2,10 @@
 
 export HISTTIMEFORMAT="%d/%m/%y %T "
 export HISTCONTROL=ignoreboth:erasedups
-export HISTSIZE=8192
-export HISTFILESIZE=8192
+export HISTFILESIZE=1000000000
+export HISTSIZE=1000000000
+export SAVEHIST=1000000000
+export HISTFILE=~/.zsh_history
+setopt HIST_FIND_NO_DUPS
+setopt inc_append_history
+setopt share_history
