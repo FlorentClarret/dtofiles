830b326c08bb30ca2331727efe66229b053a6ebe
Florent Clarret <florent.clarret@gmail.com>
2022-11-24 23:06:27 +0100

    chore(graphite): add shebang

--- home/dot_oh-my-zsh/custom/graphite.zsh
+++ home/dot_oh-my-zsh/custom/graphite.zsh
@@ -1,3 +1,5 @@
+#!/usr/bin/env zsh
+
 #compdef gtetion                                                                                                                                                                                                                                                      ─╯
 ###-begin-gt-completions-###
 #
